50509666c356b05b2029d0f33c754d95df3e7e3b
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2018-12-18 20:47:10 -0600

    changed dataType to jsonp

--- assets/javascript/results.js
+++ assets/javascript/results.js
@@ -172,7 +172,7 @@ $(".btn-floating").on("click", function() {
     $.ajax({
         url: directionsURL,
         method: "GET",
-        dataType: "json",
+        dataType: "jsonp",
         crossDomain: true,
         contentType: "application/json",
         secure: true,
